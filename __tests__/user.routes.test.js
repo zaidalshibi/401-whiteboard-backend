eec4f6237360cd6d05ebcfcb83cae57ebaa500f4
Zaid alshibi <zaidealshibi@gmail.com>
2022-09-18 16:13:43 +0300

    deleting the user login tests

--- __tests__/user.routes.test.js
+++ __tests__/user.routes.test.js
@@ -4,39 +4,6 @@ const supertest = require( 'supertest' );
 const server = require( '../server' );
 const request = supertest( server.app );
 
-describe( 'Test User login route', () => {
-    it( 'Login a user', async () => {
-        const res = await request.post( '/signin' ).send( {
-            username: 'zaidalshibi',
-            password: 'password'
-        } );
-        expect( res.status ).toEqual( 200 );
-    } );
-
-    it( 'Login a user with wrong password', async () => {
-        const res = await request.post( '/signin' ).send( {
-            username: 'zaidalshibi',
-            password: 'password123'
-        } );
-        expect( res.status ).toEqual( 401 );
-    } );
-
-    it( 'Login a user with wrong username', async () => {
-        const res = await request.post( '/signin' ).send( {
-            username: 'zaidalshibii',
-            password: 'password'
-        } );
-        expect( res.status ).toEqual( 401 );
-    } );
-
-    it( 'Login a user with wrong username and password', async () => {
-        const res = await request.post( '/signin' ).send( {
-            username: 'zaidalshibii',
-            password: 'password123'
-        } );
-        expect( res.status ).toEqual( 401 );
-    } );
-} );
 
 describe( 'Test User signup route', () => {
     it( 'Signup a user', async () => {
